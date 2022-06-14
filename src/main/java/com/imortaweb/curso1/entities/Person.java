e2cf7d06acad90bb98aa063b502d4f0ce168d704
Denis Roque <dennisimortal47@gmail.com>
2022-06-14 18:52:28 -0300

    H2 database, test profile, jpa

--- src/main/java/com/imortaweb/curso1/entities/Person.java
+++ src/main/java/com/imortaweb/curso1/entities/Person.java
@@ -3,21 +3,27 @@ package com.imortaweb.curso1.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable  { 
-	
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Person implements Serializable  { 
 	private static final long serialVersionUID = 1L;
 	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public User() {
+	public Person() {
 	}
 
-	public User(Long id, String name, String email, String phone, String password) {
+	public Person(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -79,7 +85,7 @@ public class User implements Serializable  {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Person other = (Person) obj;
 		return Objects.equals(id, other.id);
 	}	
 
--- src/main/java/com/imortaweb/curso1/resources/PersonResource.java
+++ src/main/java/com/imortaweb/curso1/resources/PersonResource.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.imortaweb.curso1.entities.User;
+import com.imortaweb.curso1.entities.Person;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/persons")
+public class PersonResource {
 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+	public ResponseEntity<Person> findAll(){
+		Person u = new Person(1L, "Maria", "maria@gmail.com", "99999999", "12345");
 		return ResponseEntity.ok().body(u);
 				
 	}
