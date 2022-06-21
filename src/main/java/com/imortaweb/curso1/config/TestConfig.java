708a8bce28b3d7a2037b4db7cd237db28517a4f3
Denis Roque <dennisimortal47@gmail.com>
2022-06-21 16:50:05 -0300

    Payment, one-to-one association

--- src/main/java/com/imortaweb/curso1/config/TestConfig.java
+++ src/main/java/com/imortaweb/curso1/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.imortaweb.curso1.entities.Category;
 import com.imortaweb.curso1.entities.Order;
 import com.imortaweb.curso1.entities.OrderItem;
+import com.imortaweb.curso1.entities.Payment;
 import com.imortaweb.curso1.entities.Person;
 import com.imortaweb.curso1.entities.Product;
 import com.imortaweb.curso1.entities.enums.OrderStatus;
@@ -82,5 +83,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
