dab2e05cd776b9aa6c64e0a31baa80c453328e16
Denis Roque <dennisimortal47@gmail.com>
2022-06-21 19:46:06 -0300

    Exception handling - delete

--- src/main/java/com/imortaweb/curso1/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/imortaweb/curso1/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.imortaweb.curso1.secrvices.exceptions.DatabaseException;
 import com.imortaweb.curso1.secrvices.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,5 +23,15 @@ public class ResourceExceptionHandler {
 				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
 
 }
