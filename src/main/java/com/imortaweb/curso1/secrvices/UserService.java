cf263d793d36558983d2f199b494c53b830f05c6
Denis Roque <dennisimortal47@gmail.com>
2022-06-21 18:36:41 -0300

    User update

--- src/main/java/com/imortaweb/curso1/secrvices/UserService.java
+++ src/main/java/com/imortaweb/curso1/secrvices/UserService.java
@@ -11,24 +11,38 @@ import com.imortaweb.curso1.repositores.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-		
-	public List<Person> findAll(){
-		return  repository.findAll();
+
+	public List<Person> findAll() {
+		return repository.findAll();
 	}
-	
+
 	public Person findById(Long id) {
-	  Optional<Person> obj = repository.findById(id);
-	  return obj.get();
+		Optional<Person> obj = repository.findById(id);
+		return obj.get();
 	}
-	
+
 	public Person insert(Person obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	@SuppressWarnings("deprecation")
+	public Person update(Long id, Person obj) {
+		Person entity = repository.getOne(id);
+		updadeData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updadeData(Person entity, Person obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
 	}
 }
